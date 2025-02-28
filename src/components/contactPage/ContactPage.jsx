cbb0d055597f1dedaa0dca2a79d5e392c17c7d6a
Anders Hellerud <andershellerud@hotmail.no>
2025-02-28 18:42:09 +0100

    separated stylefile and main file

--- src/components/contactPage/ContactPage.jsx
+++ src/components/contactPage/ContactPage.jsx
@@ -1,68 +1,14 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import styled from 'styled-components';
+import {
+  FormContainer,
+  Form,
+  Label,
+  Input,
+  TextArea,
+  SubmitButton,
+  ErrorMessage
+} from './ContactPage.styled';
 
-const FormContainer = styled.div`
-  max-width: 600px;
-  margin: 50px auto;
-  padding: 20px;
-  background-color: #f9f9f9;
-  border-radius: 10px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-`;
-
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-`;
-
-const Label = styled.label`
-  margin-bottom: 8px;
-  font-weight: bold;
-  color: #333;
-`;
-
-const Input = styled.input`
-  padding: 10px;
-  margin-bottom: 20px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  font-size: 16px;
-`;
-
-const TextArea = styled.textarea`
-  padding: 10px;
-  margin-bottom: 20px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  font-size: 16px;
-  resize: vertical;
-`;
-
-const SubmitButton = styled.button`
-  padding: 15px;
-  background-color: #28a745;
-  color: white;
-  font-size: 18px;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: #218838;
-  }
-
-  &:disabled {
-    background-color: #ccc;
-    cursor: not-allowed;
-  }
-`;
-
-const ErrorMessage = styled.p`
-  color: red;
-  font-size: 14px;
-  margin-top: -15px;
-  margin-bottom: 20px;
-`;
 
 const ContactPage = () => {
   const [fullName, setFullName] = useState('');
